485131d9bf220908766f48195a25a86b87908f4b
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-05-21 12:01:54 -0500

    Create a calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,47 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 import { Navbar } from "../components/Navbar";
+
+import enUS from 'date-fns/locale/en-US';
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+
+const locales = {
+    'en-US': enUS,
+}
+  
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [{
+    title: 'Cumpleaños de Sassie',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours( new Date(), 2),
+    bgColor: '#fafafa',
+    user:{
+        _id: '123',
+        name: 'Alejandro'
+    }
+}]
 
 export const CalendarPage = () => {
     return (
         <>
             <Navbar/>
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+            />
         </>
     )
 }
